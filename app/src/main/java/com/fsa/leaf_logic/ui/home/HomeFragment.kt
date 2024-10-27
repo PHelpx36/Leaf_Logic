5b5fe6eb8e64658108638822f2b41cc4276b6f0f
PHelpx36 <victoramadeulima.br@gmail.com>
2024-10-27 08:10:38 -0300

    dinamismo

--- app/src/main/java/com/fsa/leaf_logic/ui/home/HomeFragment.kt
+++ app/src/main/java/com/fsa/leaf_logic/ui/home/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.fsa.leaf_logic.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.marginBottom
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -35,6 +38,38 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         // Use binding para acessar a View corretamente
         binding.plusSign.setOnClickListener {
             findNavController().navigate(R.id.action_nav_home_to_nav_novaPlanta)
+        }
+
+        val dynamicContainer = binding.dynamicContainer
+
+        // Adicionar componentes dinamicamente
+        for (i in 1..3) {
+            val textView = TextView(requireContext())
+
+            if(i != 3){
+                textView.apply {
+                    text = "Notification $i"
+                    textSize = 25f
+                    setTextColor(Color.WHITE)
+                    setBackgroundResource(R.drawable.retangular_shape) // Aplicando o drawable
+                    setPadding(8, 40, 8, 40)
+                }
+            }
+            else{
+                textView.apply {
+                    text = "ver mais"
+                    textSize = 25f
+                    setTextColor(Color.WHITE)
+                    setBackgroundResource(R.drawable.retangular_shape) // Aplicando o drawable
+                    setPadding(8, 40, 8, 40)
+                    setOnClickListener {
+                        findNavController().navigate(R.id.action_nav_home_to_nav_slideshow)
+                    }
+                }
+            }
+
+            // Adiciona os componentes ao LinearLayout
+            dynamicContainer.addView(textView)
         }
 
         return binding.root
